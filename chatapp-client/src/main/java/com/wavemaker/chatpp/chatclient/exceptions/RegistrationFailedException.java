e170ddcfa372c473576e4ed9e9163e413e45bf8f
sainihal <sainihal13@gmail.com>
2016-07-24 15:46:09 +0530

    Add files via upload

---
+++ chatapp-client/src/main/java/com/wavemaker/chatpp/chatclient/exceptions/RegistrationFailedException.java
@@ -0,0 +1,12 @@
+package com.wavemaker.chatpp.chatclient.exceptions;
+
+/**
+ * Created by sainihala on 13/7/16.
+ */
+public class RegistrationFailedException extends RuntimeException {
+    public RegistrationFailedException(String message) {
+        super(message);
+    }
+
+}
+
